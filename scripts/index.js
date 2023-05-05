f07af7243aedd18313b51640e22d536c6e03c3db
nmohammednawaz <nmohammednawaz786@gmail.com>
2023-05-05 19:23:48 +0530

    Portfolio first push

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+let navbar = document.getElementById("nav-menu");
+let menu = document.getElementById("menu");
+
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
