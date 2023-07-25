8ccb4127abd14e67badcdd4b59095fcdc0c2576f
nmohammednawaz <nmohammednawaz786@gmail.com>
2023-07-25 23:02:11 +0530

    Portfolio Navbar Home and about

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,41 @@
-// Js for sticky navbar
-let navbar = document.getElementById("nav-menu");
-let menu = document.getElementById("menu");
-let header = document.getElementById("header");
+const resumeLinkOne = document.getElementById("resume-link-1");
+// const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('nav-menu');
 
-header.classList.add("glowing-border");
-window.onscroll = function(){
-    if(window.pageYOffset >= menu.offsetTop){
-        navbar.classList.add("sticky");
-        navbar.classList.add("glowing-border");
-        header.classList.remove("glowing-border");
-    }else{
-        navbar.classList.remove("sticky");
-        navbar.classList.remove("glowing-border");
-        header.classList.add("glowing-border");
-    }
+function downloadAndOpenResume(){
+
 }
 
+resumeLinkOne.addEventListener("click", function (event) {
+    event.preventDefault();
+    var url = this.href;
+    window.open(url, "_blank");
+    var fileName = "MOHAMMED-NAWAZ-RESUME.pdf";
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+    link.click();
+});
+    
+// Function to toggle the sticky class on the navbar
+function toggleStickyNavbar() {
+    if (window.scrollY > navbar.offsetTop) {
+    navbar.classList.add('sticky');
+    } else {
+    navbar.classList.remove('sticky');
+    }
+}
+  
+// Listen for scroll events and apply the sticky class accordingly
+window.addEventListener('scroll', toggleStickyNavbar);
+  
 // Js for Auto typed text   
 var typed = new Typed('#auto-typing-text', {
     strings: ["Full Stack Web Developer", "Java Backend Developer"],
-    typeSpeed: 70,
+    typeSpeed: 100,
     backSpeed: 50,
     loop: true
 });
+
+  
+  
